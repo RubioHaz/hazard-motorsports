9322ec9a9c7c79a76722fd0bbea7c2982a8cf6cf
Tyson Hazard <tysonhazard@gmail.com>
2019-02-21 18:05:14 -0500

    Reorder migrations to comply with references.

--- db/migrate/20190221185332_add_admin_email_first_name_last_name_username_avatar_subscribed_to_user.rb
+++ db/migrate/20190221185332_add_admin_email_first_name_last_name_username_avatar_subscribed_to_user.rb
@@ -1,7 +1,6 @@
 class AddAdminEmailFirstNameLastNameUsernameAvatarSubscribedToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :admin, :boolean
-    add_column :users, :email, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :username, :string
--- db/migrate/20190221210003_create_purchased_builds.rb
+++ db/migrate/20190221210003_create_purchased_builds.rb
@@ -3,7 +3,6 @@ class CreatePurchasedBuilds < ActiveRecord::Migration[5.2]
     create_table :purchased_builds do |t|
       t.integer :build_price
       t.integer :build_quantity
-      t.references :build, foreign_key: true
       t.string :build_name
       t.text :build_description
       t.references :order, foreign_key: true
