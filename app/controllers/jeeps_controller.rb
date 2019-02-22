11db854ab0b230cd220cacb9bb2f93a60668ebfc
Tyson Hazard <tysonhazard@gmail.com>
2019-02-21 19:43:27 -0500

    Create Jeeps controller, add jeep logo to navbar

---
+++ app/controllers/jeeps_controller.rb
@@ -0,0 +1,24 @@
+class JeepsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
